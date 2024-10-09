f4bbff38016f006135bdd9d69a4028fe938bc97f
mancuoj <mancuoj@gmail.com>
2024-10-09 19:03:45 +0800

    feat: decorate inject singleton class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Elysia } from 'elysia'
 import { swagger } from '@elysiajs/swagger'
 
+class Note {
+  constructor(public data: string[] = ['hi']) {}
+}
+
 const app = new Elysia()
   .use(swagger())
-  .get('/', ({ path }) => path)
-  .post('/hello', 'Do you miss me?')
+  .decorate('note', new Note())
+  .get('/note', ({ note }) => note.data)
   .listen(3000)
 
-console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
+console.log(`Running at http://${app.server?.hostname}:${app.server?.port}`)
